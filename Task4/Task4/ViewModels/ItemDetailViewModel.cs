6fbadaea25bedc63bdb86b875947f942fdcd97e9
for just <justforawork8@gmail.com>
2018-12-17 00:51:30 +0200

    Add project files.

---
+++ Task4/Task4/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Task4.Models;
+
+namespace Task4.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
